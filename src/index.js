ba1bd879d2612cc6d81b2a1919b94677e55c7789
Logan Moon <52093401+moonseokho85@users.noreply.github.com>
2020-10-11 11:55:47 +0000

    middle check

---
+++ src/index.js
@@ -0,0 +1,12 @@
+const plus = document.getElementById("add")
+const minus = document.getElementById("minus")
+const number = document.querySelector("span")
+
+let count = 0
+
+const handleAdd = () => {}
+
+const handleMinus = () => {}
+
+plus.addEventListener("click", handleAdd)
+minus.addEventListener("click", handleMinus)
