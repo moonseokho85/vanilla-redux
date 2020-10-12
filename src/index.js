480c2bbe1007172695666efa1bbe9d82fd66f60b
Logan Moon <mseokho7733@gmail.com>
2020-10-12 11:09:40 +0900

    Until now, there was plus-minus counter

--- src/index.js
+++ src/index.js
@@ -6,11 +6,14 @@ const number = document.querySelector("span");
 
 number.innerText = 0;
 
+const ADD = "ADD";
+const MINUS = "MINUS";
+
 const countModifier = (count = 0, action) => {
   switch (action.type) {
-    case "ADD":
+    case ADD:
       return count + 1;
-    case "MINUS":
+    case MINUS:
       return count - 1;
     default:
       return count;
@@ -26,11 +29,11 @@ const onChange = () => {
 countStore.subscribe(onChange);
 
 const handleAdd = () => {
-  countStore.dispatch({ type: "ADD" });
+  countStore.dispatch({ type: ADD });
 };
 
 const handleMinus = () => {
-  countStore.dispatch({ type: "MINUS" });
+  countStore.dispatch({ type: MINUS });
 };
 
 add.addEventListener("click", handleAdd);
